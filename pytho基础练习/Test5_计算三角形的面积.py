66cd42e715b69a6c2df2408d0377b3265922da36
IT-dwh <379681485@qq.com>
2018-12-15 16:29:49 +0800

    python代码练习提交

--- pytho基础练习/Test5_计算三角形的面积.py
+++ pytho基础练习/Test5_计算三角形的面积.py
@@ -6,3 +6,12 @@
 __author__ = 'dwh'
 
 # 创建日期 2018/12/14 17:16
+a = float(input("输入三角形第一边长:"))
+b = float(input("输入三角形第二边长:"))
+c = float(input("输入三角形第三边长:"))
+
+s = (a+b+c)/2
+
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+
+print("area: %0.2f" % area)
--- pytho基础练习/Test6_随机数生成.py
+++ pytho基础练习/Test6_随机数生成.py
@@ -6,3 +6,6 @@
 __author__ = 'dwh'
 
 # 创建日期 2018/12/14 17:17
+import random
+
+print(random.randint(0,9))
