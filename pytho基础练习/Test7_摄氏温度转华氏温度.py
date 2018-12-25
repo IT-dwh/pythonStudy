9352a7c2b2e79799431bca60246aab71c2185486
IT-dwh <379681485@qq.com>
2018-12-25 23:09:48 +0800

    测试tkinter

--- pytho基础练习/Test7_摄氏温度转华氏温度.py
+++ pytho基础练习/Test7_摄氏温度转华氏温度.py
@@ -6,3 +6,16 @@
 __author__ = 'dwh'
 
 # 创建日期 2018/12/14 17:17
+
+a = None
+
+
+def test():
+    # global a
+    a = 100
+    print(a)
+
+
+print(a)
+test()
+print(a)
--- tkinter/tkinter_label.py
+++ tkinter/tkinter_label.py
@@ -6,3 +6,38 @@
 __author__ = 'dwh'
 
 # 2018/12/18 22:00
+
+# import tkinter as tk
+from tkinter import StringVar
+
+dwh = StringVar()
+# tex.set("hello")
+
+# flag = False
+#
+#
+# def click1():
+#     print("hello")
+#     global flag
+#     if flag:
+#         flag = False
+#         var.set("a")
+#     else:
+#         flag = True
+#         var.set("b")
+#
+#
+# # 设置最外层容器的参数
+# tkContainer = tk.Tk("tkContainer")
+# tkContainer.title("title")
+# tkContainer.geometry("200x100")
+#
+# # 生成一个Label
+# tkLabel = tk.Label(tkContainer, textvariable=var, background="green")
+# tkLabel.pack()
+#
+# # 生成一个Button
+# tkButton = tk.Button(tkContainer, text="click me", command=click1)
+# tkButton.pack()
+#
+# tkContainer.mainloop()
